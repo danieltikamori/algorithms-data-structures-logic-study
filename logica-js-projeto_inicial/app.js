f6b40d9ee17b7a0ed9428dc4aff858bf72b49903
Daniel Tikamori <tikamori.itiro@gmail.com>
2024-01-20 21:58:48 +0900

    Add while loop until user guess the number

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,19 @@
 alert("Welcome to the Guessing Number Game!");
 
-let numeroSecreto = 29;
-let chute = prompt("Choose a number between 1 to 10");
+let secretNumber = 29;
+let guessNumber;
+let numAttempts = 1;
 
-if (chute == numeroSecreto) {
-  alert(`You discovered the secret number ${numeroSecreto}!`);
-} else {
-  alert("You mistaken the number!");
+while (guessNumber != secretNumber) {
+  guessNumber = prompt("Choose a number between 1 to 10");
+  if (guessNumber == secretNumber) {
+    alert(`You discovered the secret number ${secretNumber} in ${numAttempts} attempts!`);
+  } else {
+    if (guessNumber > secretNumber) {
+      alert(`The secret number is lower than ${guessNumber}`);
+    } else {
+      alert(`The secret number is higher than ${guessNumber}`);
+    }
+    numAttempts++;
+  }
 }
